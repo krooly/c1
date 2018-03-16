c11fa0a442089733b528c77e76ed5e64c8bab5a9
krooly <kda.krooly@gmail.com>
2018-03-16 20:30:54 +0300

    Update app.application.sh

--- app.application.sh
+++ app.application.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 echo "Hello World"
+
+mkdir ~/tmpC1/
+cd ~/tmpC1/
+
+wget https://raw.githubusercontent.com/krooly/c1/master/app.application
+
+cat app.application
+
+cd ..
+rm -rf ~/tmpC1/
